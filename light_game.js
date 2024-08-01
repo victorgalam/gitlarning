7baf1c2c43bce7ca327e22d89cff9784e9aee4b4
victorgalam <122699492+victorgalam@users.noreply.github.com>
2024-08-01 19:27:32 +0300

    Update light_game.js

--- light_game.js
+++ light_game.js
@@ -7,14 +7,4 @@ import time
        print("המנורה כבויה ⚫")
 
     def main():
-        while True:
-            user_input = input("הקלד 'דלוק' כדי להדליק את המנורה או 'כבה' כדי לכבות אותה (או 'צא' כדי לצאת): ").strip().lower()
-            if user_input == "דלוק":
-               turn_on_led()
-            elif user_input == "כבה":
-                 turn_off_led()
-            elif user_input == "צא":
-                  print("יציאה מהתוכנית.")
-                break
-            else:
-                print("קלט לא תקין, נסה שוב.")  
+    
