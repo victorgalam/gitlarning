4babccfae558cb37ff8281cd5702b40500073f34
victorgalam <122699492+victorgalam@users.noreply.github.com>
2024-08-01 19:28:33 +0300

    Update simplegame.py

--- simplegame.py
+++ simplegame.py
@@ -11,3 +11,17 @@ if guess == number_to_guess:
     print("נכון! ניחשת את המספר!")
 else:
     print(f"טעות! המספר הנכון היה {number_to_guess}. נסה שוב בפעם הבאה.")
+
+
+
+    while True:
+            user_input = input("הקלד 'דלוק' כדי להדליק את המנורה או 'כבה' כדי לכבות אותה (או 'צא' כדי לצאת): ").strip().lower()
+            if user_input == "דלוק":
+               turn_on_led()
+            elif user_input == "כבה":
+                 turn_off_led()
+            elif user_input == "צא":
+                  print("יציאה מהתוכנית.")
+                break
+            else:
+                print("קלט לא תקין, נסה שוב.")  
